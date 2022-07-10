bd8b698dd7a2e372b1d2e2107737eea558579752
Yusuf  OLGUN <abd_yusuf@yahoo.com>
2022-07-10 09:21:06 +0300

    second test case

--- src/main/java/com/cybertek/day2/SpartansGetRequests.java
+++ src/main/java/com/cybertek/day2/SpartansGetRequests.java
@@ -3,28 +3,38 @@ package com.cybertek.day2;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
-import org.apache.commons.lang3.builder.ToStringExclude;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class SpartansGetRequests {
 
 
-   String baseUrl= "http://18.233.99.140:8000";
+    String baseUrl = "http://18.233.99.140:8000";
 
-@Test
-    public void test1(){
+    @Test
+    public void test1() {
 
-   Response response = RestAssured.given().accept(ContentType.JSON).when().get(baseUrl + "/api/spartans");
+        Response response = RestAssured.given().accept(ContentType.JSON).when().get(baseUrl + "/api/spartans/3");
 
-    Assertions.assertEquals(response.statusCode(),200);
-    Assertions.assertEquals(response.contentType(),"application/json");
-
-
-
-}
+        Assertions.assertEquals(response.statusCode(), 200);
+        Assertions.assertEquals(response.contentType(), "application/json");
 
 
+    }
+
+    @Test
+    public void test2(){
+        Response response = RestAssured.given().accept(ContentType.JSON).when().get(baseUrl + "/api/spartans/3");
+
+        Assertions.assertEquals(response.statusCode(),200);
+        Assertions.assertEquals(response.contentType(),"application/json");
+        Assertions.assertTrue(response.body().asString().contains("Fidole"));
+
+
+
+
+
+    }
 
 
 }
