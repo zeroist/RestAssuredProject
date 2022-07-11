f4e54d4f366cf059d44449130a30cd45aff83e0f
Yusuf  OLGUN <abd_yusuf@yahoo.com>
2022-07-11 18:32:53 +0300

    configurationReader added

---
+++ src/main/java/com/cybertek/utilities/ConfigurationReader.java
@@ -0,0 +1,25 @@
+package com.cybertek.utilities;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+public class ConfigurationReader {
+
+    private static Properties properties=new Properties();
+
+    static{
+        try {
+            FileInputStream file=new FileInputStream("configuration.properties");
+
+            properties.load(file);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static String getProperties(String keyValue) {
+        return (String) properties.get(keyValue);
+    }
+}
---
+++ src/main/java/com/cybertek/utilities/SpartanTestBase.java
@@ -0,0 +1,14 @@
+package com.cybertek.utilities;
+
+import org.junit.jupiter.api.BeforeAll;
+
+import static io.restassured.RestAssured.baseURI;
+
+public abstract class SpartanTestBase {
+
+    @BeforeAll
+    public static void init() {
+        baseURI = ConfigurationReader.getProperties("SpartanbaseURI");
+
+    }
+}
