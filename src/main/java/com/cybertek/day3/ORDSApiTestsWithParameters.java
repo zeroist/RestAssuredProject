316a9acff6fb909aef6fd911675b7fbd731221b3
Yusuf  OLGUN <abd_yusuf@yahoo.com>
2022-07-11 12:06:42 +0300

    q with params tested

--- src/main/java/com/cybertek/day3/ORDSApiTestsWithParameters.java
+++ src/main/java/com/cybertek/day3/ORDSApiTestsWithParameters.java
@@ -1,6 +1,34 @@
 package com.cybertek.day3;
 
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.*;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class ORDSApiTestsWithParameters {
+    @BeforeAll
+    public static void init() {
+
+        baseURI = "http://18.233.99.140:1000/ords/hr";
+
+    }
+
+    @Test
+    public void test1(){
+        Response response = given().header("Content-Type", "application/json").
+                param("q", "{\"region_id\": 2}")
+                .when().get("/countries");
+
+        assertEquals(200,response.getStatusCode());
+        assertEquals("application/json",response.header("content-type"));
+        assertTrue(response.body().asString().contains("United States of America"));
+
+
+
+    }
 
 
 }
