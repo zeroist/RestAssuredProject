e349077ff84003d2aa7aac592254de690f77d475
Yusuf  OLGUN <abd_yusuf@yahoo.com>
2022-07-18 11:48:20 +0300

    ORDSApiWithJsonPath class is ready

---
+++ src/main/java/com/cybertek/day4/ORDSApiWithJsonPath.java
@@ -0,0 +1,26 @@
+package com.cybertek.day4;
+
+import com.cybertek.utilities.HRTestBase;
+import com.cybertek.utilities.HRTestBase;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static io.restassured.RestAssured.*;
+import static org.junit.jupiter.api.Assertions.*;
+public class ORDSApiWithJsonPath extends HRTestBase {
+
+    @Test
+    public void test1(){
+
+
+
+
+    }
+
+
+
+
+}
