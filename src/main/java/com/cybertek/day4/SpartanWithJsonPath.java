cb8e617672babaa511bc7d26a14575e1c7b30b6e
Yusuf  OLGUN <abd_yusuf@yahoo.com>
2022-07-18 10:58:19 +0300

    SpartanWithJsonPath was added

---
+++ src/main/java/com/cybertek/day4/SpartanWithJsonPath.java
@@ -0,0 +1,17 @@
+package com.cybertek.day4;
+
+import com.cybertek.utilities.SpartanTestBase;
+import org.junit.jupiter.api.Test;
+
+public class SpartanWithJsonPath extends SpartanTestBase {
+
+
+    @Test
+    public void test(){
+
+
+
+
+
+    }
+}
